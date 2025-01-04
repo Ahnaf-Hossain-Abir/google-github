556beca2a61160c2629f7dfc3042e046d4962cf2
Ahnaf-Hossain-Abir <mdahnafhossainabir@gmail.com>
2025-01-04 14:55:40 +0600

    trying

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 export const {handlers, signIn, signOut, auth} = NextAuth({
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     providers:[
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
